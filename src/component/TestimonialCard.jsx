14a69ef958daa21c5f84fa7e583fbf8ec985b6c4
Bobby Aleem <bobbybeatmaker@hotmail.com>
2023-03-25 23:02:23 +0500

    Update TestimonialCard.jsx

--- src/component/TestimonialCard.jsx
+++ src/component/TestimonialCard.jsx
@@ -3,30 +3,34 @@ import React from 'react'
 import FormatQuoteIcon from '@mui/icons-material/FormatQuote';
 
 function TestimonialCard({ data }) {
+
+  const styles = {
+    wrapper: 'bg-secondary flex flex-col items-start justify-start gap-4 p-8 rounded-xl shadow-md',
+    description: 'text-xl font-[400] text-[#858585] leading-[30px] text-justify',
+    quotation: '',
+    contentContainer: 'flex items-center gap-8',
+    image: 'w-[5rem] h-[5rem] object-cover rounded-full',
+    bioContainer: 'flex flex-col gap-1',
+    name: 'text-2xl font-[500] text-primary900',
+    profession: 'text-[1rem] text-[#858585]',
+  }
+
   return (
-    <div className="bg-secondary flex flex-col items-start justify-start gap-4 px-10 py-8 rounded-xl">
+    <div className={styles.wrapper}>
       <div className="">
-        <h5 className='text-xl font-[400] text-[#858585] leading-[30px] text-justify'>
-          <FormatQuoteIcon sx={{
-            fontSize: '12rem',
-            zIndex: '2',
-            color: '#4ac4f3',
-            marginBottom: '-20rem',
-            marginLeft: '-2rem',
-            position: 'relative',
-            top: '-12rem',
-            left: '1rem',
-          }}/>
+        <h5 className={styles.description}>
+          <span className={styles.quotation}>&ldquo;</span>
           {data.description}
+          <span className={styles.quotation}>&rdquo;</span>
         </h5>
       </div>
-      <div className="flex items-center gap-8">
+      <div className={styles.contentContainer}>
         <div className="">
-          <img src={data.imageUrl} alt="user-image" className='w-[5rem] h-[5rem] object-cover rounded-full' />
+          <img src={data.imageUrl} alt="user-image" className={styles.image} />
         </div>
-        <div className="flex flex-col gap-1">
-          <h5 className='text-2xl font-[500] text-primary900'>{data.name}</h5>
-          <p className='text-[1rem] text-[#858585]'>{data.profession}</p>
+        <div className={styles.bioContainer}>
+          <h5 className={styles.name}>{data.name}</h5>
+          <p className={styles.profession}>{data.profession}</p>
         </div>
       </div>
     </div>
